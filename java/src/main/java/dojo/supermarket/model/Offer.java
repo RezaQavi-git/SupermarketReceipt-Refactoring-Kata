6f01495809fe7a457edc9cacbfa926bd533195a9
Reza Qavi <rezaqavi1378@gmail.com>
2022-01-07 23:45:17 +0330

    Remove duplicate codes

--- java/src/main/java/dojo/supermarket/model/Offer.java
+++ java/src/main/java/dojo/supermarket/model/Offer.java
@@ -14,6 +14,7 @@ public class Offer {
     Product getProduct() {
         return this.product;
     }
+
     public void handleThreeForTwoOffer(Product p, double totalPrice, int quantityAsInt, double unitPrice){
         int x = 3;
         int numberOfXs = quantityAsInt / x;
@@ -23,24 +24,16 @@ public class Offer {
             p.setDiscountAmount(-discountAmount);
         }
     }
-    public void handleTwoForAmountoOffer(Product p, double totalPrice, int quantityAsInt, double unitPrice){
-        int x = 2;
+
+    public void handleForAmountOffer(Product p, double totalPrice, int quantityAsInt, double unitPrice, int x){
         int numberOfXs = quantityAsInt / x;
-        if (quantityAsInt >= 2) {
-            double discountAmount = totalPrice - (this.argument * numberOfXs + quantityAsInt % 2 * unitPrice);
-            p.setDiscountDescription("2 for " + this.argument);
+        if (quantityAsInt >= x) {
+            double discountAmount = totalPrice - (this.argument * numberOfXs + quantityAsInt % x * unitPrice);
+            p.setDiscountDescription(x + " for " + this.argument);
             p.setDiscountAmount(-discountAmount);
         }
     }
-    public void handleFiveForAmountOffer(Product p, double totalPrice, int quantityAsInt, double unitPrice){
-        int x = 5;
-        int numberOfXs = quantityAsInt / x;
-        if (quantityAsInt >= 5) {
-            double discountAmount = totalPrice - (this.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
-            p.setDiscountDescription("5 for " + this.argument);
-            p.setDiscountAmount(-discountAmount);
-        }
-    }
+
     public void handleTenPercentDiscountOffer(Product p, double totalPrice){
         p.setDiscountDescription(this.argument + "% off");
         p.setDiscountAmount(-totalPrice * this.argument / 100.0);
@@ -56,9 +49,9 @@ public class Offer {
         if(this.offerType == SpecialOfferType.ThreeForTwo) {
             handleThreeForTwoOffer(p, totalPrice, quantityAsInt, unitPrice);
         } else if(this.offerType == SpecialOfferType.TwoForAmount) {
-            handleTwoForAmountoOffer(p, totalPrice, quantityAsInt, unitPrice);
+            handleForAmountOffer(p, totalPrice, quantityAsInt, unitPrice, 2);
         } else if(this.offerType == SpecialOfferType.FiveForAmount) {
-            handleFiveForAmountOffer(p, totalPrice, quantityAsInt, unitPrice);
+            handleForAmountOffer(p, totalPrice, quantityAsInt, unitPrice, 5);
         } else if(this.offerType == SpecialOfferType.TenPercentDiscount) {
             handleTenPercentDiscountOffer(p, totalPrice);
         }
